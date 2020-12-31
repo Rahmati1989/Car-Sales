5731294acf5977b26f41589e8d454e85df9fc223
Rafiullah Rahmati <rafiullahrahmati@Rafiullahs-MacBook-Pro.local>
2020-12-30 23:41:54 -0800

    Completed the MVP

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
 
-import 'bulma/css/bulma.css';
-import './styles.scss';
 
-const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+
+import mainReducer from "./reducers/mainReducer";
+
+import App from "./App";
+
+import "bulma/css/bulma.css";
+import "./styles.scss";
+
+const store = createStore(mainReducer);
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootElement
+);
